627bbbce5e8d09f1f22d6ed530a1117987ac168a
jpatag <112610699+jpatag@users.noreply.github.com>
2025-03-28 10:24:47 -0400

    Update MainContent.js

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -19,41 +19,6 @@ import RssFeedRoundedIcon from '@mui/icons-material/RssFeedRounded';
 import { useState, useEffect } from 'react';
 import LanguageProficiencyDropdown from './LanguageProficiencyDropdown';
 
-const SyledCard = styled(Card)(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  padding: 0,
-  height: '100%',
-  backgroundColor: (theme.vars || theme).palette.background.paper,
-  '&:hover': {
-    backgroundColor: 'transparent',
-    cursor: 'pointer',
-  },
-  '&:focus-visible': {
-    outline: '3px solid',
-    outlineColor: 'hsla(210, 98%, 48%, 0.5)',
-    outlineOffset: '2px',
-  },
-}));
-
-const SyledCardContent = styled(CardContent)({
-  display: 'flex',
-  flexDirection: 'column',
-  gap: 4,
-  padding: 16,
-  flexGrow: 1,
-  '&:last-child': {
-    paddingBottom: 16,
-  },
-});
-
-const StyledTypography = styled(Typography)({
-  display: '-webkit-box',
-  WebkitBoxOrient: 'vertical',
-  WebkitLineClamp: 2,
-  overflow: 'hidden',
-  textOverflow: 'ellipsis',
-});
 
 function Author({ authors }) {
   return (
@@ -130,14 +95,6 @@ export default function MainContent() {
     France: 'fr',
     Germany: 'de',
     USA: 'en',
-  };
-
-  const handleFocus = (index) => {
-    setFocusedCardIndex(index);
-  };
-
-  const handleBlur = () => {
-    setFocusedCardIndex(null);
   };
 
   // Fetch articles based on the selected country
